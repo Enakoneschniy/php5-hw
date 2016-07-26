dd20491178166e0dd075911c931fb9f1559f41b6
Dima Glamazda <gldmvl@gmail.com>
2016-07-26 13:08:28 +0300

    HomeWork for two classes

--- Glamazda_Dmitry/02/task_3/index.php
+++ Glamazda_Dmitry/02/task_3/index.php
@@ -7,7 +7,7 @@
 
     if($a < $c){
        $x = $a + $b / $c * $a;
-    }else if($a == $c){
+    }else if($a == 10){
         $x = 100;
     }else if($a > $c){
         $x = $c * (3 * $b) + $c * sqrt($c);
