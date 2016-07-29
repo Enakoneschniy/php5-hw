3ab96ce70aaf18464f2810b72146d426c40890b5
lysakkonstantin <lysak-kostik@mail.ru>
2016-07-29 14:16:40 +0300

    Hw03

---
+++ lysak_konstantin/02/index01.php
@@ -0,0 +1,24 @@
+<?php
+$name = "Konstantin";
+$age = 28;
+echo "Меня зовут $name <br/>";
+
+echo "Мне $age лет";
+echo "<hr/>";
+
+$a = 5;
+$S = 1/4 * $a * (sqrt(3));
+echo $S;
+echo "<hr/>";
+
+$a = rand(0, 10);
+$b = rand(0, 10);
+$c = rand(0, 10);
+if ($a<$c){
+    echo $x = $a+$b/$c*$a;
+}elseif ($a = 10) {
+    echo $x = 100;
+}elseif ($a>$c){
+    echo $c*(3*$b) + $c/$c*(sqrt($c));
+};
+?>
