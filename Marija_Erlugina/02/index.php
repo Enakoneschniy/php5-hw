821efffca26fdda750872999d9d0065d5c30d82c
Marija Erlugina <absidiana11@gmail.com>
2016-07-29 22:38:30 +0300

    03- 2

--- Marija_Erlugina/02/index.php
+++ Marija_Erlugina/02/index.php
@@ -1,27 +1,95 @@
 <?php
 
-echo "Привет Мир!<br /><br />";
+define('PI', 3.14);
 
-// ***************** Issue #1 *******************
+const NAME = "Evgeniy";
 
-echo "<h2>Задача 1</h2>";
 
-$name = "Marija";
-$age = 27;
+echo PI,"<br />", NAME, "<br />";  // обращение к константам
 
-echo "Меня зовут " . $name . "<br />";
-echo "Мне " . $age . " лет";
+echo __FILE__, "<br />";  // стандартные константы
+echo __DIR__, "<br />";
+echo __LINE__;
 
-// ***************** Issue #2 *******************
 
-echo "<h2>Задача 2</h2>";
+$t="0";
+echo"t=$t".gettype($t)."<br>";
+$t+=2;
+echo"t=$t".gettype($t)."<br>";
+$t=$t+3.5;
+echo"t=$t".gettype($t)."<br>";
+$t=5+"5поросят";
+echo"t=$t".gettype($t)."<br>";
+$t=5.0+"5поросят";
+echo"t=$t".gettype($t)."<br>";
 
-$a = 5;
-$square = (pow($a,2) * sqrt(3))/4;
 
-echo "Пусть сторона треугольника равна " . $a . "<br />";
+$t="Всем";//переменнойtприсваиваемзначение"Всем"
+$$t="привет!";//Переменной"Всем"присваиваемзначение"привет!"
+echo "$$t ".$$t."<br>";
+echo "$t ${$t}<br>";
+echo "$t $Всем";
 
-echo "Площадь равностороннего треугольника равна " . $square;
+//**********************
+
+echo "<hr>";
+
+$t1 = 96;
+
+$t2= &$t1;
+
+$t3= $t1;
+
+echo "t1 = $t1; <br> t2 = $t2 <br>";
+
+$t1 = 315;
+
+echo $t2 . "- t2, а t3 равно " . $t3;
+
+
+
+
+
+$a = 2;
+$b = 5;
+
+if ($a < $b) {
+    echo '<hr>';
+    echo '$a < $b <br>';
+    echo '<hr>';
+    echo "$a < $b";
+}
+
+
+if ( $a > $b xor $b == 3 ) {
+    echo "True";
+}
+else {
+    echo "False";
+}
+
+echo "<hr>";
+
+if ($a < $b):
+
+    echo "gfdgdgf";
+
+else :
+
+    echo "fd";
+
+    endif;
+
+echo "<hr>";
+
+echo $a < $b ? 'true' : 'false';
+
+
+
+
+
+
+
 
 
 
