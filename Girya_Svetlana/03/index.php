7e52ecde3342113c0f0947a03d5918258b1bceb4
Girya Svetlana <sveta_girya@mail.ru>
2016-07-29 23:21:55 +0300

    hw 03

--- Girya_Svetlana/03/index.php
+++ Girya_Svetlana/03/index.php
@@ -1,10 +1,26 @@
 <?php
+//задание 1
 $name="Светлана";
 $age="26";
 echo "Меня зовут $name";
 echo '<hr>';
 echo "Мне $age лет";
 echo '<hr>';
+//задание 2
 $a=6;
 $S = 1/4 * $a * sqrt(3);
 echo "1/4 * $a * sqrt(3) = $S";
+echo '<hr>';
+//задание 3
+$b=15;
+$c=7;
+if($a < $c){
+    $X=$a+$b/$c*$a;
+}
+else if($a==10){
+    $X=100;
+}
+else if($a >$c){
+    $X = $c*(3*$b) +$c/$c *sqrt($c);
+}
+echo "X=$X";
