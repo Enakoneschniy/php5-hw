73283cecaf26dcc155e50db4534ec8f71b5a2e29
Alexey Kobyleha <uazonner@gmail.com>
2016-07-30 00:35:02 +0300

    Home Work 03

--- Alexey_Kobyleha/lesson_02/index.php
+++ Alexey_Kobyleha/lesson_02/index.php
@@ -6,14 +6,14 @@ echo "<h3>Task №1</h3>";
 $name = "Алексей";
 $age = 28;
 
-echo "Меня зовут ", $name . "<br>";
-echo "Мне " . $age . " лет<br>";
+echo "Меня зовут $name<br>";
+echo "Мне $age лет<br>";
 
 // Task №2
 echo "<h3>Task №2</h3>";
 
 function sTriangle($a) {
-	$s = 1/4*$a*sqrt(3);
+	$s = 1%4*$a*sqrt(3);
 	return $s;
 }
 
@@ -21,42 +21,17 @@ $s = sTriangle(20);
 echo "Площадь равностороннего треугольника S = " . $s;
 
 // Task №3v.1
-echo "<h3>Task №3v.1 (c*<font color='red'>3*b</font>+c/c*√c)</h3>";
-
-function xEquallyV1($a, $b, $c) {
-	if ($a == 10) {
-		$x = 100;
-		return $x;
-	} elseif (($a < $c) && ($a != 10)) {
-		$x = $a+$b/$c*$a;
-		return $x;
-	} elseif (($a > $c) && ($a != 10)) {
-		$x = $c*3*$b+$c/$c*sqrt($c);
-		return $x;
-	} else {
-		$x = "Что-то пошло не так (a=c)";
-		return $x;
-	}
-}
-
-$x = xEquallyV1(25, 15, 20);
-echo "X = ", $x;
-
-// Task №3v.2 - Crutches ;)
-echo "<h3>Task №3v.2 (c*<font color='red'>3b</font>+c/c*√c)</h3>";
+echo "<h3>Task №3 (c*3*b+c/c*√c)</h3>";
 
 function xEqually($a, $b, $c) {
 	if ($a == 10) {
 		$x = 100;
 		return $x;
-	} elseif (($a < $c) && ($a != 10)) {
-		$x = $a+$b/$c*$a;
+	} elseif ($a < $c && $a != 10) {
+		$x = $a+$b%$c*$a;
 		return $x;
-	} elseif (($a > $c) && ($a != 10)) {
-		settype($b, "string");
-		$b = "3" . substr($b,0);
-		settype($b, "integer");
-		$x = $c*$b+$c/$c*sqrt($c);
+	} elseif ($a > $c && $a != 10) {
+		$x = $c*3*$b+$c%$c*sqrt($c);
 		return $x;
 	} else {
 		$x = "Что-то пошло не так (a=c)";
@@ -65,6 +40,4 @@ function xEqually($a, $b, $c) {
 }
 
 $x = xEqually(25, 15, 20);
-echo "X = ", $x;
-
-?>
+echo "X = $x";
