6e985699b500691ad4d3c3de0dcdd35f6520eb8b
Dima Kovalskiy <koval-rip@mail.ru>
2016-07-29 20:31:41 +0300

    Homework for third lesson

---
+++ Dmitriy_Kovalskiy/Test/2.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>Использование динамической переменной</title>
+</head>
+<body>
+<div style = "margin: 50px 150px; text-align: center; background: aqua; font-size: 14pt; color: saddlebrown; font-family: arial;">
+<?php
+$price = array("vegetables", "fruits");
+print_r ($price);
+
+    ?>
+</div>
+</body>
+</html>
